762380439c92cbdffd9f37cc7d3cb0aafaa441ac
ritvikvermaa <vritvik8@gmail.com>
2026-07-16 19:42:48 +0530

    Learned datatypes and type casting

---
+++ Foundation/DataTypes.java
@@ -0,0 +1,49 @@
+package Foundation;
+
+public class DataTypes {
+  public static void main(String[] args) {
+    
+    // Numeric Data Types
+
+    byte num1 = 127;
+    System.out.println(num1);
+
+    short num2 = 32767;
+    System.out.println(num2);
+
+    int num3 = 50000;
+    System.out.println(num3);
+
+    long num4 = 734492778;
+    System.out.println(num4);
+
+    // Floating Data Types
+
+    float num5 = 3.146578f;
+    System.out.println(num5);
+
+    double num6 = 3.146589268474883;
+    System.out.println(num6);
+
+    // Other - Char, Boolean
+
+    boolean eligibleToVote = true;
+    System.out.println(eligibleToVote);
+
+    char firstCharacter = 'a';
+    System.out.println("My first character is " + firstCharacter);
+
+    // Implicit Type Casting
+
+    byte oldNum = 15;
+    int newNum = oldNum;
+    System.out.println("New Number: " + newNum);
+
+    // Explicit Type Casting
+
+    long value1 = 3789296473992764;
+    int value2 = (int)value1;
+    System.out.println("New Value: " + value2);
+
+  }
+}
