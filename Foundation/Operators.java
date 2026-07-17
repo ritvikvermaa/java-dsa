8e8a280bc456fd5a938b156c6e502bdad90bcb38
ritvikvermaa <vritvik8@gmail.com>
2026-07-17 10:12:15 +0530

    Learned relational operators

--- Foundation/Operators.java
+++ Foundation/Operators.java
@@ -1,23 +1,31 @@
 package Foundation;
 public class Operators{
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+    // Arithmetic Operators
+    int solvedThisWeek = 10;
+    int solvedLastWeek = 2;
+    int total = solvedLastWeek + solvedThisWeek;
+    int difference = solvedThisWeek + solvedLastWeek;
+    int projected = solvedThisWeek * 4;
+    int average = solvedThisWeek / 7;
+    int remainder = solvedThisWeek % 7;
+    System.out.println(total);
+    System.out.println(difference);
+    System.out.println(projected);
+    System.out.println(average);
+    System.out.println(remainder);
     
-// Arithmetic Operators
+    // Relational Operators
 
-int solvedThisWeek = 10;
-int solvedLastWeek = 2;
+    int currentStreak = 45;
+    int targetStreak = 50;
 
-int total = solvedLastWeek + solvedThisWeek;
-int difference = solvedThisWeek + solvedLastWeek;
-int projected = solvedThisWeek * 4;
-int average = solvedThisWeek / 7;
-int remainder = solvedThisWeek % 7;
-
-System.out.println(total);
-System.out.println(difference);
-System.out.println(projected);
-System.out.println(average);
-System.out.println(remainder);
-
-  }
+    System.out.println(currentStreak == targetStreak);
+    System.out.println(currentStreak != targetStreak);
+    System.out.println(currentStreak > targetStreak);
+    System.out.println(currentStreak < targetStreak);
+    System.out.println(currentStreak >= targetStreak);
+    System.out.println(currentStreak <= targetStreak);
+    
+    }
 }
