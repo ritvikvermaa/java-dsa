7495a1268422c29d5bfc3b81beab96e945a77b5f
ritvikvermaa <vritvik8@gmail.com>
2026-07-17 09:56:15 +0530

    Learned arithmetic operators

---
+++ Foundation/Operators.java
@@ -0,0 +1,23 @@
+package Foundation;
+public class Operators{
+  public static void main(String[] args) {
+    
+// Arithmetic Operators
+
+int solvedThisWeek = 10;
+int solvedLastWeek = 2;
+
+int total = solvedLastWeek + solvedThisWeek;
+int difference = solvedThisWeek + solvedLastWeek;
+int projected = solvedThisWeek * 4;
+int average = solvedThisWeek / 7;
+int remainder = solvedThisWeek % 7;
+
+System.out.println(total);
+System.out.println(difference);
+System.out.println(projected);
+System.out.println(average);
+System.out.println(remainder);
+
+  }
+}
